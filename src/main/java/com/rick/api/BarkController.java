3c8f0c527f16a025715ee4a206dd19a5754ad95c
rick <jkxyx205@163.com>
2026-01-18 19:16:19 +0800

    refactor: 代码优化验证参数

--- src/main/java/com/rick/api/BarkController.java
+++ src/main/java/com/rick/api/BarkController.java
@@ -3,10 +3,8 @@ package com.rick.api;
 import com.rick.notification.bark.PushNotification;
 import com.rick.notification.bark.PushNotificationService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.apache.commons.lang3.StringUtils;
+import org.springframework.web.bind.annotation.*;
 
 /**
  * @author Rick.Xu
@@ -21,15 +19,17 @@ public class BarkController {
     private final PushNotificationService pushNotificationService;
 
     @GetMapping
-    public void message(String title, String subtitle, String body) {
-        pushNotificationService.push(PushNotification.builder()
-                .title(title)
-                .subtitle(subtitle)
-                .body(body)
-                .badge(1)
-                .sound("alarm")
-                .icon("https://day.app/assets/images/avatar.jpg")
-                .group("default")
-                .build());
+    public void message(@RequestParam String title, String subtitle, @RequestParam String body) {
+        if (StringUtils.isNotBlank(title) && StringUtils.isNotBlank(body)) {
+            pushNotificationService.push(PushNotification.builder()
+                    .title(title)
+                    .subtitle(subtitle)
+                    .body(body)
+                    .badge(1)
+                    .sound("alarm")
+                    .icon("https://day.app/assets/images/avatar.jpg")
+                    .group("default")
+                    .build());
+        }
     }
 }
